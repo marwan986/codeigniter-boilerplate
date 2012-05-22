3c98d0eaeb675ec8050414422bc7cc268018a285
marwan986 <aziz.marwa@gmail.com>
2012-05-22 00:19:28 -0400

    stable black

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -1,14 +1,32 @@
 <div class="wrap">
+
+        <nav id="topNav">
+        <ul>
+            <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">about</a></li>
+            <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">products</a></li>
+            <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">clients</a></li>
+            <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">contact</a></li>
+        </ul>
+        </nav>
+   
 <div id="logo">
     
     <img alt="mbs_logo" src="<?php echo base_url().'images/mbs_logo.jpg' ?>" />
+    <div id="logo-desc">
+        <h3> Middlesex Business Supplies </h3>
+        <h4> paper roll king since 1977 </h4>
+    </div>
+    
+    
 </div>
-
-<nav>
+    <div id="lineBreaker">
+        
+    </div>
+<nav id="mainNav">
     <ul>
-        <li class="<?php echo isActive($page_id,"homepage")?>"><a href="<?php echo  base_url()?>homepage">home</a></li>
-        <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">products</a></li>
-        <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">clients</a></li>
+        <li class="<?php echo isActive($page_id,"homepage")?>"><a href="<?php echo  base_url()?>homepage">Restaurants</a></li>
+        <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">Grocery Stores</a></li>
+        <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">QSR</a></li>
         <li class="<?php echo isActive($page_id,"example")?>"><a href="<?php echo base_url()?>example">contact</a></li>
     </ul>
 </nav>
